d70d1adc4d03cd9c046caadc5a70789c7de411c8
Arnav Singh <arnav010singh@gmail.com>
2024-03-17 20:07:10 +0530

    minor fix 2

--- index.js
+++ index.js
@@ -52,3 +52,5 @@ app.use("/", require("./routes/index.js"));
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+console.log("Server Started");
